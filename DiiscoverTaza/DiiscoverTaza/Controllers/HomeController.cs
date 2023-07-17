1d56b6c0bf0ff67e59e980ee61cc3355a687848f
walidlakbaiil <lakbailwalid01@gmail.com>
2023-07-17 11:09:57 +0000

    1st Version of the project

---
+++ DiiscoverTaza/DiiscoverTaza/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using DiiscoverTaza.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DiiscoverTaza.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
