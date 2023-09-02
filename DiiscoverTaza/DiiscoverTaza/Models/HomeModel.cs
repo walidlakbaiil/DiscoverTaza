632b489b27e499ec117fd1a69e2d9d52ddb535b1
Haddouchesara <haddouchesara89@gmail.com>
2023-09-02 12:36:06 +0100

    SaraBranch
    Art andCulture_Hotels

---
+++ DiiscoverTaza/DiiscoverTaza/Models/HomeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DiiscoverTaza.Models
+{
+    public class FormData
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Subject { get; set; }
+        public string Question { get; set; }
+    }
+
+}
